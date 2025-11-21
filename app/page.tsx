cbc61818011f0372c765eb581144f2f5a54791db
lever-labs <thewiretapai@gmail.com>
2025-11-21 16:31:14 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home(): React.ReactNode {
 				warpAmount={0.5}
 			/>
 			<div className="absolute inset-0 flex items-center justify-center">
-				<h1 className="text-4xl font-bold text-white">Hello World</h1>
+				<h1 className="text-4xl font-bold text-white">Wiretap</h1>
 			</div>
 		</div>
 	)
