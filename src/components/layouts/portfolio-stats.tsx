b1209adebad48b6de5cdffe9ce90b8ca1ae57639
Ariel Tamayev <actamayev@gmail.com>
2025-12-08 20:36:08 -0500

    Update portfolio-stats.tsx

--- src/components/layouts/portfolio-stats.tsx
+++ src/components/layouts/portfolio-stats.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-import { useMemo } from "react"
 import { observer } from "mobx-react"
 import { cn } from "../../lib/utils"
 import fundsClass from "../../classes/funds-class"
@@ -13,43 +12,36 @@ interface PortfolioStats {
 }
 
 function PortfolioStats(): React.ReactNode {
-	const selectedFund = useMemo((): SingleFund | undefined => {
-		if (!fundsClass.selectedFundUuid) {
-			return undefined // We'll handle this case below
+	// MobX observer will track observable changes
+	const selectedFund = fundsClass.selectedFundUuid
+		? fundsClass.funds.get(fundsClass.selectedFundUuid)
+		: undefined
+
+	const allFunds = Array.from(fundsClass.funds.values())
+
+	let portfolioStats: PortfolioStats
+
+	if (selectedFund) {
+		// Show data for selected fund only
+		portfolioStats = {
+			totalFunds: 1,
+			totalBalance: selectedFund.currentAccountBalanceUsd,
+			totalStartingBalance: selectedFund.startingAccountBalanceUsd,
+			totalPnL: selectedFund.currentAccountBalanceUsd - selectedFund.startingAccountBalanceUsd
 		}
-		return fundsClass.funds.get(fundsClass.selectedFundUuid)
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [fundsClass.selectedFundUuid, fundsClass.funds.size])
-
-	const allFunds = useMemo((): SingleFund[] => {
-		return Array.from(fundsClass.funds.values())
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [fundsClass.funds.size])
-
-	const portfolioStats = useMemo((): PortfolioStats => {
-		if (selectedFund) {
-			// Show data for selected fund only
-			return {
-				totalFunds: 1,
-				totalBalance: selectedFund.currentAccountBalanceUsd,
-				totalStartingBalance: selectedFund.startingAccountBalanceUsd,
-				totalPnL: selectedFund.currentAccountBalanceUsd - selectedFund.startingAccountBalanceUsd
-			}
-		}
-
+	} else {
 		// Show data for all funds combined
 		const totalBalance = allFunds.reduce((sum: number, fund: SingleFund): number =>
 			sum + fund.currentAccountBalanceUsd, 0)
 		const totalStartingBalance = allFunds.reduce((sum: number, fund: SingleFund): number =>
 			sum + fund.startingAccountBalanceUsd, 0)
-
-		return {
+		portfolioStats = {
 			totalFunds: allFunds.length,
 			totalBalance,
 			totalStartingBalance,
 			totalPnL: totalBalance - totalStartingBalance
 		}
-	}, [selectedFund, allFunds])
+	}
 
 	// TODO: Calculate positions value from actual positions data
 	// For now, using placeholder - positions would need to be fetched for each fund
